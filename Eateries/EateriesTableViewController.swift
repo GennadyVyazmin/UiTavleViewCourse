ec71ffb595a22aff635a4ca70e435bffa3ce484f
Геннадий Вязьмин <prlme@MacBook-Pro-Gennadij.local>
2018-12-18 00:10:52 +0300

    OOP(arrays restaurants in struct

--- Eateries/EateriesTableViewController.swift
+++ Eateries/EateriesTableViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class EateriesTableViewController: UITableViewController {
-    
-    var restaurantNames = ["Ogonёk Grill&Bar", "Елу", "Bonsai", "Дастархан", "Индокитай", "X.O", "Балкан Гриль", "Respublica", "Speak Easy", "Morris Pub", "Вкусные истории", "Классик", "Love&Life", "Шок", "Бочка"]
-    
-    var restaurantImages = ["ogonek.jpg", "elu.jpg", "bonsai.jpg", "dastarhan.jpg", "indokitay.jpg", "x.o.jpg", "balkan.jpg", "respublika.jpg", "speakeasy.jpg", "morris.jpg", "istorii.jpg", "klassik.jpg", "love.jpg", "shok.jpg", "bochka.jpg"]
-    
-    var restorauntIsVisited = [Bool](repeating: false, count: 15)
+    var restaurants: [Restaurant] = [
+        Restaurant(name: "Ogonёk Grill&Bar", type: "ресторан", location: "Уфа", image: "ogonek.jpg", isVisited: false),
+        Restaurant(name: "Елу", type: "ресторан", location: "Уфа", image: "elu.jpg", isVisited: false),
+        Restaurant(name: "Bonsai", type: "ресторан", location: "Уфа", image: "bonsai.jpg", isVisited: false),
+        Restaurant(name: "Дастархан", type: "ресторан", location: "Уфа", image: "dastarhan.jpg", isVisited: false),
+        Restaurant(name: "Индокитай", type: "ресторан", location: "Уфа", image: "indokitay.jpg", isVisited: false),
+        Restaurant(name: "X.O", type: "ресторан-клуб", location: "Уфа", image: "x.o.jpg", isVisited: false),
+        Restaurant(name: "Балкан Гриль", type: "ресторан", location: "Уфа", image: "balkan.jpg", isVisited: false),
+        Restaurant(name: "Respublica", type: "ресторан", location: "Уфа", image: "respublika.jpg", isVisited: false),
+        Restaurant(name: "Speak Easy", type: "ресторанный комплекс", location: "Уфа", image: "speakeasy.jpg", isVisited: false),
+        Restaurant(name: "Morris Pub", type: "ресторан", location: "Уфа", image: "morris.jpg", isVisited: false),
+        Restaurant(name: "Вкусные истории", type: "ресторан", location: "Уфа", image: "istorii.jpg", isVisited: false),
+        Restaurant(name: "Классик", type: "ресторан", location: "Уфа", image: "klassik.jpg", isVisited: false),
+        Restaurant(name: "Love&Life", type: "ресторан", location: "Уфа", image: "love.jpg", isVisited: false),
+        Restaurant(name: "Шок", type: "ресторан", location: "Уфа", image: "shok.jpg", isVisited: false),
+        Restaurant(name: "Бочка", type: "ресторан", location:  "Уфа", image: "bochka.jpg", isVisited: false),
+        Restaurant(name: "Piplz", type: "Bar", location: "Blaga", image: "", isVisited: true)]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -34,19 +46,21 @@ class EateriesTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return restaurantNames.count
+        return restaurants.count
     }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! EateriesTableViewCell
         
-        cell.thumbnailImageView.image = UIImage(named: restaurantImages[indexPath.row])
+        cell.thumbnailImageView.image = UIImage(named: restaurants[indexPath.row].image)
         cell.thumbnailImageView.layer.cornerRadius = 32.5
         cell.thumbnailImageView.clipsToBounds = true
-        cell.nameLabel.text = restaurantNames[indexPath.row]
+        cell.nameLabel.text = restaurants[indexPath.row].name
+        cell.locationLabel.text = restaurants[indexPath.row].location
+        cell.typeLabel.text = restaurants[indexPath.row].type
         
-        cell.accessoryType =  self.restorauntIsVisited[indexPath.row] ? .checkmark : .none
+        cell.accessoryType =  self.restaurants[indexPath.row].isVisited ? .checkmark : .none
         
         return cell
     }
@@ -96,16 +110,14 @@ class EateriesTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
         
         let share = UITableViewRowAction(style: .default, title: "Share") { (action, indexPath) in
-            let defaultText = "I am in " + self.restaurantNames[indexPath.row]
-            if let image = UIImage(named: self.restaurantImages[indexPath.row]){
+            let defaultText = "I am in " + self.restaurants[indexPath.row].name
+            if let image = UIImage(named: self.restaurants[indexPath.row].image){
                 let activityController = UIActivityViewController(activityItems: [defaultText, image], applicationActivities: nil)
                 self.present(activityController, animated: true, completion: nil)
             }
         }
         let delete = UITableViewRowAction(style: .default, title: "Delete") { (action, indexPath) in
-            self.restorauntIsVisited.remove(at: indexPath.row)
-            self.restaurantImages.remove(at: indexPath.row)
-            self.restaurantNames.remove(at: indexPath.row)
+            self.restaurants.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .automatic )
             
         }
@@ -117,7 +129,7 @@ class EateriesTableViewController: UITableViewController {
         if segue.identifier == "detailSegue" {
             if let indexPath = tableView.indexPathForSelectedRow {
                 let dvc = segue.destination as! DetailViewController
-                dvc.imageName = self.restaurantImages[indexPath.row]
+                dvc.imageName = self.restaurants[indexPath.row].image
             }
         }
     }
---
+++ Eateries/restaurants.swift
@@ -0,0 +1,17 @@
+//
+//  Restraurants.swift
+//  Eateries
+//
+//  Created by Геннадий Вязьмин on 17/12/2018.
+//  Copyright © 2018 Ivan Akulov. All rights reserved.
+//
+
+import Foundation
+
+struct Restaurant {
+    let name: String
+    let type: String
+    let location: String
+    let image: String
+    let isVisited: Bool
+}
