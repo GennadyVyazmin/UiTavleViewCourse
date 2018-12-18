cdd184b7a95c55dc4443fdb4b65fb718dfd10401
Геннадий Вязьмин <prlme@MacBook-Pro-Gennadij.local>
2018-12-18 16:33:08 +0300

    Design Detail VC part 1

---
+++ Eateries/Detail/DetailTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  DetailTableViewCell.swift
+//  Eateries
+//
+//  Created by Геннадий Вязьмин on 18/12/2018.
+//  Copyright © 2018 Ivan Akulov. All rights reserved.
+//
+
+import UIKit
+
+class DetailTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var keyLabel: UILabel!
+    @IBOutlet weak var valueLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
